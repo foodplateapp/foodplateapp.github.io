2efef9a4bfab8cc5c7d28eacc52b306e66c3a5ca
foodplateapp <58179063+foodplateapp@users.noreply.github.com>
2020-11-28 20:29:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+    apiKey: "AIzaSyAv5to_ZRqKxfwJ5uD0T4nIUQRU8K6MVbo",
+    authDomain: "foodplate-b906e.firebaseapp.com",
+    databaseURL: "https://foodplate-b906e.firebaseio.com",
+    projectId: "foodplate-b906e",
+    storageBucket: "foodplate-b906e.appspot.com",
+    messagingSenderId: "331791703798",
+    appId: "1:331791703798:web:335a702046dfd5a88f96d7",
+    measurementId: "G-DGMD8WHWWY"
+  };
+firebase.initializeApp(config);
+
+var CLIENT_ID =
+    '331791703798-3jKheWHeP9cEK5B3ckHrkvfAAEH3.firebaseapp.com';
